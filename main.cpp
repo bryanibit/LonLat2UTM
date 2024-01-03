70fe3d2e275a200095ea156e61843af3d4e3f8d8
bryan <bryanibit@gmail.com>
2024-01-03 10:15:05 +0800

    add cmakelists

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include "utm.h"
 #include <iostream>
+#include <iomanip>
 int main() {
 	int ReferenceEllipsoid = 23;
-	double Lat = 40.3545;
-	double Long = 115.3489;
+	double Lat = 39.756131;
+	double Long = 116.015991;
 	double UTMNorthing;
 	double UTMEasting;
 	char *UTMZone = new char[10];
 	LLtoUTM(ReferenceEllipsoid, Lat, Long,
 		UTMNorthing, UTMEasting, UTMZone);
+	UTMNorthing += 35;
+	UTMEasting += 35;
 	double lat, lon;
 	UTMtoLL(ReferenceEllipsoid, UTMNorthing, UTMEasting, UTMZone, lat, lon);
-	std::cout << "lat, lon via two transform:" << lat << "," << lon << std::endl;
+	std::cout << "lat, lon via two transform:" << std::setprecision(10) << lat << "," << lon << std::endl;
 	return 0;
 }
